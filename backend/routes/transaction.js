d9c0d78163fd80778879c4ac9fa5655642b46277
Kaan <kaanasutay10@gmail.com>
2024-10-11 00:45:50 +0300

    create expense

--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -3,6 +3,7 @@
     EXPENSE TRACKER APP
 ------------------------------------------------------- */
 const { addIncome, getIncome, deleteIncome } = require('../controllers/income.js')
+const { addExpense, getExpense, deleteExpense } = require('../controllers/expense.js')
 
 const router = require('express').Router()
 
@@ -10,5 +11,8 @@ const router = require('express').Router()
 router.post('/add-income', addIncome)
       .get('/get-income', getIncome)
       .delete('/delete-income/:id', deleteIncome)
+      .post('/add-expense', addExpense)
+      .get('/get-expense', getExpense)
+      .delete('/delete-expense/:id', deleteExpense)
 
 module.exports = router
