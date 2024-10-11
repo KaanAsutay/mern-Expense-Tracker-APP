30ff8e04cb120fda2700624d2eeb83856df05fd8
Kaan <kaanasutay10@gmail.com>
2024-10-12 00:23:31 +0300

    create styled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-function App() {
-  return (
-    <div className='App'>
+import styled from "styled-components";
+import bg from './img/bg.png';
+import { MainLayout } from './styles/Layout'
 
-    </div>
+function App() { 
+
+  return (
+    <AppStyled bg={bg} className='App'>
+      <MainLayout>
+        <h1></h1>
+      </MainLayout>
+    </AppStyled>
   )
 }
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg});
+  position: relative;
+  `;
 
 export default App;
