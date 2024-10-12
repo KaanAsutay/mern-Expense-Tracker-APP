c7f85485c311965c9d4860f40d24e1c8420c6975
Kaan <kaanasutay10@gmail.com>
2024-10-13 00:11:48 +0300

    add globalContext in index.js

--- frontend/src/Components/Income/Income.js
+++ frontend/src/Components/Income/Income.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import { InnertLayout } from '../../styles/Layout';
+import { useGlobalContext } from '../../context/globalContext';
 
 function Income() {
+    const {addIncome} = useGlobalContext()
   return (
     <IncomeStyled>
       <InnertLayout>
