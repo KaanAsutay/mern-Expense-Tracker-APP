642be2c81a31a68ae313881f6bd2dc2d1e2df453
Kaan <kaanasutay10@gmail.com>
2024-10-13 00:17:41 +0300

    create form.js and add title

--- frontend/src/Components/Form/Form.js
+++ frontend/src/Components/Form/Form.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
 
 function Form() {
+    const [inputState, setInputState] = useState({
+        title: '',
+        amount: '',
+        date: '',
+        category: '',
+        description: '',
+    })
+
+    const { title, amount, date, category, description } = inputState;
+
+    const handleInput = name => e => {
+        setInputState({...inputState, [name]: e.target.value})
+    }
+
   return (
-    <div>
+    <FormStyled>
+
+      <div className='input-control'>
+        <input 
+        type='text' 
+        value={title}
+        name={'title'}
+        placeholder='Salary Title'
+        onChange={handleInput('title')}
+        />
+      </div>
       
-    </div>
+    </FormStyled>
   )
 }
+
+const FormStyled = styled.form`
+
+`;
 
 export default Form
