90df682b8215834d87b25d74b7cea81b8274a67d
Kaan <kaanasutay10@gmail.com>
2024-10-14 01:17:35 +0300

    add total Income and complete ıncome everything

--- frontend/src/Components/Income/Income.js
+++ frontend/src/Components/Income/Income.js
@@ -6,7 +6,7 @@ import Form from '../Form/Form';
 import IncomeItem from '../IncomeItem/IncomeItem';
 
 function Income() {
-    const {addIncome, incomes, getIncomes, deleteIncome} = useGlobalContext()
+    const {addIncome, incomes, getIncomes, deleteIncome, totalIncome} = useGlobalContext()
 
     useEffect(() => {
       getIncomes()
@@ -16,8 +16,8 @@ function Income() {
     <IncomeStyled>
       <InnertLayout>
         <h1>Incomes</h1>
+        <div className='total-income'>Total Income: <span>${totalIncome()}</span></div>
         <div className='income-content'>
-
           <div className='form-container'>
             <Form />
           </div>
@@ -47,6 +47,26 @@ function Income() {
 const IncomeStyled = styled.div`
   display: flex;
   overflow: auto;
+
+  .total-income{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #FCF6F9;
+    border: 2px solid #FFFFFF;
+    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+    border-radius: 20px;
+    padding: 1rem;
+    margin: 1rem 0;
+    font-size: 2rem;
+    gap: .5rem;
+
+    span{
+      font-size: 2rem;
+      font-weight: 800;
+      color: var(--color-green);
+    }
+  }
 
   .income-content{
     display: flex;
