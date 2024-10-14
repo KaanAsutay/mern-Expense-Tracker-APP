02c987b1817b7b8346c4a44e19691e4b5f78ab08
Kaan <kaanasutay10@gmail.com>
2024-10-15 01:48:17 +0300

    Complete expenses page

--- frontend/src/Components/Income/Income.js
+++ frontend/src/Components/Income/Income.js
@@ -24,7 +24,7 @@ function Income() {
 
           <div className='incomes'>
             {incomes.map((income) => {
-              const {_id, title, amount, date, category, description} = income;
+              const {_id, title, amount, date, category, description, type} = income;
               return <IncomeItem 
                 key={_id}
                 id={_id}
@@ -32,6 +32,7 @@ function Income() {
                 description={description}
                 amount={amount}
                 date={date}
+                type={type}
                 category={category}
                 indicatorColor="var(--color-green)"
                 deletItem={deleteIncome}
