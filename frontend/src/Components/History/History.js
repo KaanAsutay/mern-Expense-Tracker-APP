749aa0327b77d01b2b8c881401cc120c17728174
Kaan <kaanasutay10@gmail.com>
2024-10-16 00:59:26 +0300

    add some div and style

--- frontend/src/Components/History/History.js
+++ frontend/src/Components/History/History.js
@@ -4,10 +4,35 @@ import { useGlobalContext } from '../../context/globalContext';
 
 function History() {
     const {transactionHistory} = useGlobalContext()
+
+    const [...history] = transactionHistory()
+
   return (
-    <div>
-    History
-    </div>
+    <HistoryStyled>
+        <h2>Recent History</h2>
+        {history.map((item) => {
+            const {_id, title, amount, type} = item
+            return (
+                <div key={_id} className='history-item'>
+
+                    <p style={{
+                        color: type === 'expense' ? 'red' : 'var(--color-green)'
+                    }}>
+                        {title}
+                    </p>
+
+                    <p style={{
+                        color: type === 'expense' ? 'red' : 'var(--color-green)'
+                    }}>
+                        {
+                            type === 'expense' ? `${amount}` : `+${amount}`
+                        }
+                    </p>
+
+                </div>
+            )
+        })}
+    </HistoryStyled>
   )
 }
 
