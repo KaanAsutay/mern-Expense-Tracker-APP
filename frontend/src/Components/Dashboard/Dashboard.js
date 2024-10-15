69a98f4da7eb1fcee9b259b2129d9dd91e52de3e
Kaan <kaanasutay10@gmail.com>
2024-10-15 23:33:22 +0300

    create and add chart

--- frontend/src/Components/Dashboard/Dashboard.js
+++ frontend/src/Components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { InnertLayout } from '../../styles/Layout';
+import Chart from '../Chart/Chart';
 
 function Dashboard() {
   return (
@@ -9,7 +10,7 @@ function Dashboard() {
           <h1>All Transaction</h1>
           <div className='stats-con'>
             <div className='chart-con'>
-              
+              <Chart />
             </div>
           </div>
       </InnertLayout>
